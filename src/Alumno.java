e43b07e36e0b728efe54b48defe5581fb9441864
Esk1z0 <j.esteban.rincon.m@gmail.com>
2022-02-27 18:51:17 +0100

    Ya solo queda el AlgoritmoCola por hacer

--- src/Alumno.java
+++ src/Alumno.java
@@ -1,3 +1,41 @@
 public class Alumno {
+    private String nombre;
+    private String matricula;
+    private double calificacionMedia;
+    private int numAsignaturas;
+    private Pila asignaturas;
 
+    public Alumno(String nombre, String matricula){
+        this.nombre = nombre;
+        this.matricula = matricula;
+        this.calificacionMedia = 0.0;
+        this.asignaturas = new Pila();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public double getCalificacionMedia() {
+        return calificacionMedia;
+    }
+    public void anadirAsignatura(Asignatura asignatura){
+        this.asignaturas.apilar(asignatura);
+        this.numAsignaturas = this.asignaturas.getNumElementos();
+        this.calificacionMedia = (this.calificacionMedia*(this.numAsignaturas - 1)+asignatura.getCalificacion())/this.numAsignaturas;
+    }
+    public int getNumAsignaturas() {
+        return numAsignaturas;
+    }
+    public void mostrarAlumno(){
+        System.out.println(this.nombre + ". Matr: " + this.matricula + " Media: (" + this.calificacionMedia + ")/nAsignaturas:/n");
+        if (this.numAsignaturas == 0){
+            System.out.println("No está matriculado en ninguna asignatura");
+        }
+        else {
+            this.asignaturas.mostrar();
+        }
+    }
 }
